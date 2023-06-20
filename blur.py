2526db9b85a57e24c2f76a872f30690d4d033f76
isaackhlam <isaac.lamkh@gmail.com>
2023-06-20 17:42:55 +0800

    move script in correct path

--- blur.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image, ImageFilter
-from pathlib import Path
-
-tmp = Image.open('/tmp/screen.png')
-blur = tmp.filter(ImageFilter.GaussianBlur(8))
-blur.save(str(Path.home()) + '/.config/swaylock/lock.png')
--- lock.sh
+++
@@ -1,7 +0,0 @@
-TMPBG=/tmp/screen.png
-BLUR=~/.config/swaylock/lock.png
-
-grim $TMPBG
-python blur.py
-swaylock -i $BLUR
-rm $TMPBG
